92d443111728a908840b8e8264380d62d8cee62e
sakshamsaharawat <sakshamjaat88@gmail.com>
2025-01-14 22:00:00 +0530

    feat : Add todo sidebar and dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all source files
+    './public/index.html',        // Include HTML files if used
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
