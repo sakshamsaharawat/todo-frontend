88c3875c4e9510963b8e2da14a191466d4bdaba1
sakshamsaharawat <sakshamjaat88@gmail.com>
2025-01-22 10:44:36 +0530

    feat : minor changes fot upcoming

--- src/pages/Upcoming/Upcoming.tsx
+++ src/pages/Upcoming/Upcoming.tsx
@@ -6,7 +6,7 @@ import { todoData } from '../../data/todoData';
 
 const Upcoming: React.FC = () => {
   return (
-    <div className='upcoming-main-component' style={{maxHeight: "100vh"}}>
+    <div className='upcoming-main-component'>
       <div className="d-flex align-item-center">
         <h1 className='main-heading-color ml-4'>Upcoming</h1>
         <div className='upcoming-notification d-flex justify-content-center mt-1 border-radius-5 b-ws heading-color'>12</div>
